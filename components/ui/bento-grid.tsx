23382272177892bb872e145096d256a1b338bd85
LsMzl-code <louismzl.dev@gmail.com>
2024-10-01 21:48:20 +0200

    trois

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./button";
 // import { Button } from "@/components/ui/button";
 
 const BentoGrid = ({
@@ -31,29 +31,33 @@ const BentoCard = ({
   description,
   href,
   cta,
+  content
 }: {
   name: string;
   className: string;
   background: ReactNode;
   Icon: any;
   description: string;
-  href: string;
-  cta: string;
+  href?: string;
+  cta?: string;
+  content?: ReactNode;
 }) => (
   <div
     key={name}
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
       // light styles
-      "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+      "bg-gray-950/[.01] [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
       // dark styles
-      "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      "transform-gpu dark:bg-gray-50/[.10] dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
       className,
     )}
   >
     <div>{background}</div>
+
+    <div className="absolute top-6 left-6 text-neutral-500 w-[80%]">{content}</div>
     <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-      <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
+      <Icon className="h-12 w-12 origin-left transform-gpu text-foreground/80 transition-all duration-300 ease-in-out group-hover:scale-75" />
       <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
         {name}
       </h3>
@@ -65,12 +69,11 @@ const BentoCard = ({
         "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
       )}
     >
-      {/* <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
-        <a href={href}>
+      <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
+        <a href={href} target="_blank">
           {cta}
-          <ArrowRightIcon className="ml-2 h-4 w-4" />
         </a>
-      </Button> */}
+      </Button>
     </div>
     <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
   </div>
